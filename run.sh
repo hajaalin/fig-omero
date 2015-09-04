5cd95ed6f823a314a8dd9a8a295144776c58b0f5
Harri Jäälinoja <harri.jaalinoja at helsinki.fi>
2015-09-04 11:51:08 +0300

    add variable to indicate dev environment when building

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 . make_env.sh
-ansible-playbook build_images.yml 
+ansible-playbook build_images.yml -e dev=true
 docker-compose -f compose/compose-DEV.yml rm
 docker-compose -f compose/compose-DEV.yml up
-
